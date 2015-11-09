6aca496eb580057eac5ef24e3ef35b66e9b65b07
Vadim Mashuk <MashukW@gmail.com>
2015-11-09 17:18:06 +0300

    AddExtensionsMethod

---
+++ Matrix/ExtensionsMethodMatrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace Matrix
+{
+    public static class ExtensionsMethodMatrix
+    {
+        public static SquareMatrix<T> Add<T>(this SquareMatrix<T> matrix, SquareMatrix<T> addMatrix)
+        {
+            if (addMatrix == null)
+                throw new AddMatrixException("Add Matrix is invalid");
+
+            if (matrix.Order != addMatrix.Order)
+                throw new AddMatrixException("The matrices are of different order");
+
+            SquareMatrix<T> resultMatrix = new SquareMatrix<T>(matrix.Order);
+            for (int i = 0; i < resultMatrix.Order; i++)
+            {
+                for (int j = 0; j < resultMatrix.Order; j++)
+                {
+                    try
+                    {
+                        resultMatrix[i, j] = (dynamic)matrix[i, j] + (dynamic)addMatrix[i, j];
+                    }
+                    catch (RuntimeBinderException e)
+                    {
+                        throw new AddMatrixException("It is impossible to perform addition of two matrices of type");
+                    }
+                }
+            }
+
+            return resultMatrix;
+        }
+    }
+}
